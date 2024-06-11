ab6cd0c637116a82b5849d171635fc0801e18dea
vivek11416 <vivek.pal@siemens-energy.com>
2024-06-11 17:12:49 +0530

    bubble sort added

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubbleSort(arr,reverse=False):
+    #[2,4,3,6,8,4,5]
+    for i in range(len(arr)-1,0,-1):
+        for j in range(0,i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+
+    return arr
+print(bubbleSort([2,4,3,6,8,4,3,4,5,199,345,5]))
+
+
